396c91198f5dce8605edea7aa1f54d423efa646f
Shirbhateritwik47 <63262876+Shirbhateritwik47@users.noreply.github.com>
2020-04-13 18:59:30 +0530

    MyCaptain_Data_structure_1

---
+++ Data_structure1.py
@@ -0,0 +1,18 @@
+#List
+mylist1 = ["C","C++",1,2,30]
+mylist1.append("python")
+mylist1.insert(0,"java")
+
+print (mylist1)
+
+#Tuple
+mytuple = (5,6,2,"PHP")
+
+print(mytuple)
+print(len(mytuple))
+
+#Dictionary
+mydict = {"Parrot":1,"Spparow":2,"Peacock":3}
+del mydict["Parrot"]
+
+print(mydict)
